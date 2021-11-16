717c1fcc2cb9112781994e1d81e01939f4e1766c
QTYO-KARAAGE <76415676+QTYO-KARAAGE@users.noreply.github.com>
2021-11-16 11:33:01 +0900

    Add files via upload

---
+++ newsekigae.c
@@ -0,0 +1,55 @@
+/*
+ 11/16/2021 8:30 am
+ THIS PROGRAM HAS FATAL BUG!
+ DO NOT USE THIS PROGRAM!
+ 
+ 11/16/2021 10:00 am
+ FIXED BUG!
+ YOU CAN USE THIS PROGRAM.
+ THANK YOU.
+ 
+ CREATED BY @QTYO_KARAAGE
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MEMBER 39
+
+int main(void)
+{
+	
+	int seat[MEMBER];
+	int number;
+	int i;
+	int x;
+	int n;
+	int seat_no;
+	
+	srand((unsigned int)time(NULL));
+	
+	for (i = 0; i < MEMBER; i++){
+		seat[i] = 0;
+	}
+	i = 0;
+	while (i < MEMBER){
+		seat_no = (rand() % MEMBER + 1);
+		
+		x = 0;
+		while(x < MEMBER){ //被りがないかチェック
+			for (n = 0; n < MEMBER; n++){
+				if(seat[n] == seat_no){
+					seat_no = (rand() % MEMBER + 1);
+					n = 0;
+				}
+			}
+			x++;
+		}
+		seat[i] = seat_no;
+		i++;
+	}
+	
+	for (i = 0; i < MEMBER; i++){
+		printf("座席番号%2d >> 出席番号%2d\n", i + 1, seat[i]);
+	}
+	return (0);
+}
